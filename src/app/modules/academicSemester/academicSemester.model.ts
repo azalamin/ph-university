7ca0c3537c265bb43b7fd2ebfa22fe3ce591b09c
Al Amin Sheikh <azalamins@gmail.com>
2023-12-24 18:49:26 +0600

    [done] semester created

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Months,
+} from "./academicSemester.constant";
+import { TAcademicSemester } from "./academicSemester.interface";
+
+const academicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      enum: AcademicSemesterName,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      enum: AcademicSemesterCode,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const AcademicSemester = model<TAcademicSemester>(
+  "AcademicSemester",
+  academicSemesterSchema,
+);
+
+export default AcademicSemester;
--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -28,7 +28,7 @@ const createStudentValidationSchema = z.object({
     student: z.object({
       name: userNameValidationSchema,
       gender: z.enum(["male", "female", "other"]),
-      dateOfBirth: z.string().optional(),
+      dateOfBirth: z.date().optional(),
       email: z.string().email({ message: "Enter a valid email" }),
       contactNo: z.string(),
       emergencyContact: z.string(),
